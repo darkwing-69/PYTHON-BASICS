d27699e4c5ebd936a5b8087bd5b7b9a1197e9bac
ARUN RAJ <136427491+darkwing-69@users.noreply.github.com>
2023-06-17 21:52:52 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,21 @@
+wthr=float(input("Enter The temperature  in celsius:"))
+if wthr>=35 and wthr<=49:
+    print("According to  this temp weather will be too hot")
+elif wthr>=30 and wthr<=34:
+    print("Accordinng to his temp  weather will be humid")
+elif wthr>=20 and wthr<=28:
+    print("According to this temp weather will be two cold")
+else:
+    print("No  records Found")
+    
+OUTPUT:
+
+Enter The temperature  in celsius:42
+According to  this temp weather will be too hot
+
+Enter The temperature  in celsius:32
+Accordinng to his temp  weather will be humid
+
+Enter The temperature  in celsius:22
+According to this temp weather will be two cold
+
