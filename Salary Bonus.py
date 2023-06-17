0cd3e902f2914e2ed11b551887bd977e153df4e4
ARUN RAJ <136427491+darkwing-69@users.noreply.github.com>
2023-06-17 22:03:33 +0530

    Add files via upload

---
+++ Salary Bonus.py
@@ -0,0 +1,20 @@
+Tar=int(input("Enter your sale count:"))
+sal=int(input("Enter your salary:"))
+if Tar<30:
+    print("Sorry no bonus better luck next time")
+elif Tar>30:
+    salr=sal+5000
+    print("Congrats you got your bonus")
+    print("your salary will be:",salr)
+
+OUTPUT:
+Enter your sale count:12
+Enter your salary:13000
+Sorry no bonus better luck next time
+
+Enter your sale count:32
+Enter your salary:20000
+Congrats you got your bonus
+your salary will be: 25000
+
+
