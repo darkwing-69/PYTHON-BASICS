db60f8a5a1ba3806b7484d18c829350dab44bc3a
ARUN RAJ <136427491+darkwing-69@users.noreply.github.com>
2023-06-17 22:25:29 +0530

    Add files via upload

---
+++ Assigning  Grade for  Marks.py
@@ -0,0 +1,19 @@
+# assigning grades (A, B, C) based on marks obtained by a student.
+
+Mark=int(input("Enter Your Marks:"))
+percent=Mark/500*100
+print("Your percentage will be:",percent)
+if percent>90:
+    print("Congrats your grade is A")
+elif percent>75:
+    print("Congrats your grade is B")
+elif percent>65:
+    print("Congrats your grade is C")
+else:
+    print("Sorry no grade assigned")
+
+OUTPUT:
+Enter Your Marks:386
+Your percentage will be: 77.2
+Congrats your grade is B
+
