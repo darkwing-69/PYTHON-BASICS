b2012af1ca8b5ad3bc704c00a586c819d9bd306f
ARUN RAJ <136427491+darkwing-69@users.noreply.github.com>
2023-06-20 08:20:29 +0530

    Add files via upload

---
+++ All Arithmetic Table.py
@@ -0,0 +1,176 @@
+#All Arithmetic Tables
+q=print('''Available Modes are:
+        1)Addition Table
+        2)Subtraction Table
+        3)Multiple Table
+        4)Expoenential Table
+        5)Division table
+        6)Floor Division Table
+        7)Modolus Table''')
+
+a=input("Enter your Mode:")
+
+if a=='3':
+   b=(int(input("Enter your Multiple Table You want:")))
+   for i in range(1,100):
+     e=b*i
+     w=print(b,'X',i,'=',e)
+     
+elif a=='2':
+    c=(int(input("Enter Your Subtraction Table You want:")))
+    for i in range(1,100):
+      e=c-i
+      w=print(c,'-',i,'=',e)
+      
+elif a=='1':
+    g=(int(input("Enter Your Addition Table You want:")))
+    for i in range(1,100):
+      e=g+i
+      w=print(g,'+',i,'=',e)
+      
+elif a=='4':
+    k=(int(input("Enter Your Exponential Table You want:")))
+    for i in range(1,100):
+      e=k**i
+      w=print(k,'**',i,'=',e)
+      
+elif a=='5':
+    L=(int(input("Enter Your Division Table You want:")))
+    for i in range(1,100):
+      e=L/i
+      w=print(L,"/",i,'=',e)
+      
+elif a=='6':
+    s=(int(input("Enter your Floor Division Table You Want:")))
+    for i in range(1,100):
+        e=s//i
+        w=print(s,"//",i,'=',e)
+        
+elif a=='7':
+    v=(int(input("Enter your Modolus Table You want:")))
+    for i in range(1,100):
+        e=v%i
+        w=print(v,'%',i,'=',e)
+    
+else:
+    print("Invalid Entry Try Again")
+
+OUTPUT:
+
+Available Modes are:
+        1)Addition Table
+        2)Subtraction Table
+        3)Multiple Table
+        4)Expoenential Table
+        5)Division table
+        6)Floor Division Table
+        7)Modolus Table
+        
+Enter your Mode:3
+
+Enter your Multiple Table You want:143
+
+143 X 1 = 143
+143 X 2 = 286
+143 X 3 = 429
+143 X 4 = 572
+143 X 5 = 715
+143 X 6 = 858
+143 X 7 = 1001
+143 X 8 = 1144
+143 X 9 = 1287
+143 X 10 = 1430
+143 X 11 = 1573
+143 X 12 = 1716
+143 X 13 = 1859
+143 X 14 = 2002
+143 X 15 = 2145
+143 X 16 = 2288
+143 X 17 = 2431
+143 X 18 = 2574
+143 X 19 = 2717
+143 X 20 = 2860
+143 X 21 = 3003
+143 X 22 = 3146
+143 X 23 = 3289
+143 X 24 = 3432
+143 X 25 = 3575
+143 X 26 = 3718
+143 X 27 = 3861
+143 X 28 = 4004
+143 X 29 = 4147
+143 X 30 = 4290
+143 X 31 = 4433
+143 X 32 = 4576
+143 X 33 = 4719
+143 X 34 = 4862
+143 X 35 = 5005
+143 X 36 = 5148
+143 X 37 = 5291
+143 X 38 = 5434
+143 X 39 = 5577
+143 X 40 = 5720
+143 X 41 = 5863
+143 X 42 = 6006
+143 X 43 = 6149
+143 X 44 = 6292
+143 X 45 = 6435
+143 X 46 = 6578
+143 X 47 = 6721
+143 X 48 = 6864
+143 X 49 = 7007
+143 X 50 = 7150
+143 X 51 = 7293
+143 X 52 = 7436
+143 X 53 = 7579
+143 X 54 = 7722
+143 X 55 = 7865
+143 X 56 = 8008
+143 X 57 = 8151
+143 X 58 = 8294
+143 X 59 = 8437
+143 X 60 = 8580
+143 X 61 = 8723
+143 X 62 = 8866
+143 X 63 = 9009
+143 X 64 = 9152
+143 X 65 = 9295
+143 X 66 = 9438
+143 X 67 = 9581
+143 X 68 = 9724
+143 X 69 = 9867
+143 X 70 = 10010
+143 X 71 = 10153
+143 X 72 = 10296
+143 X 73 = 10439
+143 X 74 = 10582
+143 X 75 = 10725
+143 X 76 = 10868
+143 X 77 = 11011
+143 X 78 = 11154
+143 X 79 = 11297
+143 X 80 = 11440
+143 X 81 = 11583
+143 X 82 = 11726
+143 X 83 = 11869
+143 X 84 = 12012
+143 X 85 = 12155
+143 X 86 = 12298
+143 X 87 = 12441
+143 X 88 = 12584
+143 X 89 = 12727
+143 X 90 = 12870
+143 X 91 = 13013
+143 X 92 = 13156
+143 X 93 = 13299
+143 X 94 = 13442
+143 X 95 = 13585
+143 X 96 = 13728
+143 X 97 = 13871
+143 X 98 = 14014
+143 X 99 = 14157
+
+
+     
+    
+    
